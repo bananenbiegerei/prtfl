39fff3444302e8328d2b0f60968712d020f962fc
Ingo Schmid <is@bananenbiegerei.de>
2023-06-12 14:23:06 +0200

    deactivated WP default groups

--- functions/acf-blocks.php
+++ functions/acf-blocks.php
@@ -3,6 +3,6 @@
 // Define this to limit which blocks will be loaded
 //define('WMDE_ALLOWED_BB_BLOCKS', ['image', 'card']);
 
-define('WMDE_ALLOWED_WP_BLOCKS', ['core/group', 'core/column', 'core/columns', 'core/html']);
+define('WMDE_ALLOWED_WP_BLOCKS', ['core/column', 'core/columns', 'core/html']);
 
 require_once get_template_directory() . '/blocks/init.php';
