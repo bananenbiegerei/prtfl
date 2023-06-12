90f179cd54ca90463a1865cae2688088ffebd7ea
Éric Leclercq <eric@naad.io>
2023-06-12 14:00:42 +0200

    Changes to work with blocks submodules

--- src/js/site.js
+++ src/js/site.js
@@ -3,11 +3,8 @@ import * as TW from './tailwindhelpers';
 import Alpine from 'alpinejs';
 import Swiper, { Navigation, Pagination, Keyboard, Autoplay, EffectFade, Thumbs } from 'swiper';
 
-import * as Accordion from './bb-blocks/accordion';
-
 window.Alpine = Alpine;
 Alpine.start();
 
 // Tests
-Accordion.test();
-console.log(TW.fullConfig);
+//console.log(TW.fullConfig);
--- tailwind.config.js
+++ tailwind.config.js
@@ -31,53 +31,55 @@ module.exports = {
 		require('@tailwindcss/aspect-ratio'),
 		require('@tailwindcss/forms'),
 		require('tailwindcss-themer')({
-		  defaultTheme: {
-			// put the default values of any config you want themed
-			// just as if you were to extend tailwind's theme like normal https://tailwindcss.com/docs/theme#extending-the-default-theme
-			extend: {
-			  // colors is used here for demonstration purposes
-			  colors: {
-				primary: {
-					'50': '#f7f7f7',
-					'100': '#e3e3e3',
-					'200': '#c8c8c8',
-					'300': '#a4a4a4',
-					'400': '#818181',
-					'500': '#666666',
-					'600': '#515151',
-					'700': '#434343',
-					'800': '#383838',
-					DEFAULT: '#000000',
+			defaultTheme: {
+				// put the default values of any config you want themed
+				// just as if you were to extend tailwind's theme like normal https://tailwindcss.com/docs/theme#extending-the-default-theme
+				extend: {
+					// colors is used here for demonstration purposes
+					colors: {
+						primary: {
+							50: '#f7f7f7',
+							100: '#e3e3e3',
+							200: '#c8c8c8',
+							300: '#a4a4a4',
+							400: '#818181',
+							500: '#666666',
+							600: '#515151',
+							700: '#434343',
+							800: '#383838',
+							900: '#000094',
+
+							DEFAULT: '#000000',
+						},
+						secondary: {
+							DEFAULT: '#f7f7f7',
+							100: '#e3e3e3',
+							200: '#c8c8c8',
+							300: '#a4a4a4',
+							400: '#818181',
+							500: '#666666',
+							600: '#515151',
+							700: '#434343',
+							800: '#383838',
+							900: '#000000',
+						},
+						focus: {
+							50: '#e7f6ff',
+							100: '#d3efff',
+							200: '#b0dfff',
+							300: '#81c8ff',
+							400: '#4fa1ff',
+							DEFAULT: '#2878ff',
+							600: '#044bff',
+							700: '#0049ff',
+							800: '#0038c4',
+							900: '#0b3aa4',
+						},
+					},
 				},
-				secondary: {
-					DEFAULT: '#f7f7f7',
-					'100': '#e3e3e3',
-					'200': '#c8c8c8',
-					'300': '#a4a4a4',
-					'400': '#818181',
-					'500': '#666666',
-					'600': '#515151',
-					'700': '#434343',
-					'800': '#383838',
-					'900': '#000000',	
-				},
-				focus: {
-					'50': '#e7f6ff',
-					'100': '#d3efff',
-					'200': '#b0dfff',
-					'300': '#81c8ff',
-					'400': '#4fa1ff',
-					DEFAULT: '#2878ff',
-					'600': '#044bff',
-					'700': '#0049ff',
-					'800': '#0038c4',
-					'900': '#0b3aa4',
-				}
-			  }
-			}
-		  },
-		  // if needed add custom themes here…
-		  // themes: [
+			},
+			// if needed add custom themes here…
+			// themes: [
 			// {
 			//   name: 'my-theme',
 			//   extend: {
@@ -86,7 +88,7 @@ module.exports = {
 			// 	}
 			//   }
 			// }
-		  // ]
-		})
+			// ]
+		}),
 	],
 };
