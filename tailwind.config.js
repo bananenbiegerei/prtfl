292fb44ddcb02d950f4367e6b65d0b5b5726ff17
Ingo Schmid <is@bananenbiegerei.de>
2023-12-01 09:30:56 +0100

    added new button and color setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -32,26 +32,23 @@ module.exports = {
 		require('@tailwindcss/forms'),
 		require('tailwindcss-themer')({
 			defaultTheme: {
-				// put the default values of any config you want themed
-				// just as if you were to extend tailwind's theme like normal https://tailwindcss.com/docs/theme#extending-the-default-theme
 				extend: {
-					// colors is used here for demonstration purposes
 					colors: {
 						primary: {
-							50: '#f7f7f7',
-							100: '#e3e3e3',
-							200: '#c8c8c8',
-							300: '#a4a4a4',
-							400: '#818181',
-							500: '#666666',
-							600: '#515151',
-							700: '#434343',
-							800: '#383838',
+							50: '#e5eeff',
+							100: '#cfe0ff',
+							200: '#a9c3ff',
+							300: '#7599ff',
+							400: '#3f5dff',
+							500: '#1423ff',
+							600: '#0008ff',
+							700: '#0009ff',
+							800: '#0008e3',
 							900: '#000094',
-
-							DEFAULT: '#000000',
+							DEFAULT: '#000068',
 						},
 						secondary: {
+							50: '#f7f7f7',
 							DEFAULT: '#f7f7f7',
 							100: '#e3e3e3',
 							200: '#c8c8c8',
@@ -69,26 +66,161 @@ module.exports = {
 							200: '#b0dfff',
 							300: '#81c8ff',
 							400: '#4fa1ff',
+							500: '#2878ff',
 							DEFAULT: '#2878ff',
 							600: '#044bff',
 							700: '#0049ff',
 							800: '#0038c4',
 							900: '#0b3aa4',
 						},
+						black: {
+							DEFAULT: 'black',
+						},
+						red: {
+							50: '#fff0f2',
+							100: '#ffe2e6',
+							200: '#ffc9d4',
+							300: '#ff9db1',
+							400: '#ff6688',
+							DEFAULT: '#ff3164',
+							600: '#f21b5a',
+							650: '#f21b5a',
+							700: '#cb0544',
+							800: '#B0003E',
+							900: '#910a3d',
+						},
+						gray: {
+							50: '#f7f7f7',
+							100: '#DCDCDC',
+							200: '#c8c8c8',
+							300: '#a4a4a4',
+							400: '#797979',
+							DEFAULT: '#F6F6F6',
+							500: '#666666',
+							600: '#515151',
+							700: '#434343',
+							800: '#383838',
+							900: '#313131',
+						},
+						green: {
+							50: '#edfcf4',
+							100: '#d2f9e2',
+							200: '#a9f1cb',
+							300: '#72e3ae',
+							400: '#39ce8e',
+							500: '#15b474',
+							600: '#0a915e',
+							DEFAULT: '#087951',
+							800: '#095c3f',
+							900: '#084c35',
+						},
+						neon: {
+							50: '#fcffe5',
+							100: '#f5ffc8',
+							DEFAULT: '#ebff9a',
+							300: '#d8fb5b',
+							400: '#c4f229',
+							500: '#a5d80a',
+							600: '#80ad03',
+							700: '#608308',
+							800: '#4d670d',
+							900: '#415710',
+						},
+						orange: {
+							50: '#fffaec',
+							100: '#fff3d3',
+							200: '#ffe4a5',
+							300: '#ffce6d',
+							400: '#ffae32',
+							500: '#ff930a',
+							DEFAULT: '#ff7b00',
+							700: '#cc5902',
+							800: '#a1450b',
+							900: '#823b0c',
+							950: '#461b04',
+						},
 					},
 				},
 			},
-			// if needed add custom themes here…
-			// themes: [
-			// {
-			//   name: 'my-theme',
-			//   extend: {
-			// 	colors: {
-			// 	  primary: 'blue'
-			// 	}
-			//   }
-			// }
-			// ]
+			themes: [
+				{
+					name: 'team-members-scheme',
+					extend: {
+						colors: {
+							palerose: {
+								DEFAULT: '#FBC5B4',
+							},
+							palepurple: {
+								DEFAULT: '#E0D3FF',
+							},
+							palepeach: {
+								DEFAULT: '#FADAB6',
+							},
+							palecyan: {
+								DEFAULT: '#D4E9DE',
+							},
+							palepink: {
+								DEFAULT: '#F7DAEF',
+							},
+							paleblue: {
+								DEFAULT: '#C0D8F4',
+							},
+							palegreen: {
+								DEFAULT: '#E3F9D6',
+							},
+							default: {
+								DEFAULT: '#d5d2d7',
+							},
+						},
+					},
+				},
+				{
+					name: 'orga-scheme',
+					extend: {
+						colors: {
+							red: {
+								DEFAULT: '#D04425',
+							},
+							cyan: {
+								DEFAULT: '#00B9FF',
+							},
+							blue: {
+								DEFAULT: '#3A25FF',
+								200: '#EEEAFF',
+							},
+							black: {
+								DEFAULT: '#000',
+							},
+						},
+					},
+				},
+				{
+					name: 'white-scheme',
+					extend: {
+						colors: {
+							primary: {
+								DEFAULT: '#fff',
+							},
+							black: {
+								DEFAULT: '#fff',
+							},
+						},
+					},
+				},
+				{
+					name: 'default-scheme',
+					extend: {
+						colors: {
+							primary: {
+								DEFAULT: '#000068',
+							},
+							black: {
+								DEFAULT: 'black',
+							},
+						},
+					},
+				},
+			],
 		}),
 	],
 };
