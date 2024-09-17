ab7eeb8d92e597b0a629a44656d4bf4888130215
bananenbiegerei <74664048+bananenbiegerei@users.noreply.github.com>
2024-09-17 14:48:23 +0200

    repsonisve footer

--- footer.php
+++ footer.php
@@ -2,11 +2,12 @@
 <footer class="flex w-full">
 <div class="transform scale-y-[-1]">
         <?php get_template_part('template-parts/wing'); ?>
-    </div>    <div class="flex-1 bg-primary p-4 flex flex-col justify-between rounded-t-abcd min-h-[160px]">
-    <div class="flex gap-2">
+    </div>
+    <div class="flex-1 bg-primary p-4 flex flex-col justify-between rounded-t-abcd sm:min-h-[160px]">
+    <div class="sm:flex gap-2">
         <div class="flex-1">
-            <p>
-                yes,this is website is accessible
+            <p class="mb-0">
+                <?php _e('yes,this is website is accessible', BB_TEXT_DOMAIN); ?>
             </p>
         </div>
         <div>
@@ -14,14 +15,14 @@
                 <?php wp_nav_menu([
                         'theme_location' => 'footer',
                         'container' => false,
-                        'items_wrap' => '<ul class="menu right">%3$s</ul>',
-                        'link_before' => '<span class="text-lg">',
+                        'items_wrap' => '<ul class="flex gap-2 sm:gap-0 sm:flex-col leading-tight items-end">%3$s</ul>',
+                        'link_before' => '<span class="sm:text-lg">',
                         'link_after' => '</span>',
                     ]); ?>
             </nav>
         </div>
     </div>
-    <div class="flex gap-2">
+    <div class="sm:flex gap-2">
         <div class="flex-1">
         <?php get_template_part( 'template-parts/logo'); ?>
         </div>
